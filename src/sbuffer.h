7c4779d54d0b5574703efc9db4dc779b8d233ece
koekeishiya <aasvi93@hotmail.com>
2018-03-31 15:49:07 +0200

    .

--- src/sbuffer.h
+++ src/sbuffer.h
@@ -14,14 +14,14 @@ struct buf_hdr
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define OFFSETOF(t, f) (size_t)((char *)&(((t *)0)->f) - (char *)0)
 
-#define buf__hdr(b) ((struct buf_hdr *)((char *)b - OFFSETOF(struct buf_hdr, buf)))
-#define buf__should_grow(b, n) (buf_len(b) + (n) > buf_cap(b))
+#define buf__hdr(b) ((struct buf_hdr *)((char *)(b) - OFFSETOF(struct buf_hdr, buf)))
+#define buf__should_grow(b, n) (buf_len(b) + (n) >= buf_cap(b))
 #define buf__fit(b, n) (buf__should_grow(b, n) ? ((b) = buf__grow_f(b, buf_len(b) + (n), sizeof(*(b)))) : 0)
 
 #define buf_len(b) ((b) ? buf__hdr(b)->len : 0)
 #define buf_cap(b) ((b) ? buf__hdr(b)->cap : 0)
-#define buf_push(b, x) (buf__fit(b, 1), b[buf_len(b)] = (x), buf__hdr(b)->len++)
-#define buf_last(b) (b[buf_len(b)-1])
+#define buf_push(b, x) (buf__fit(b, 1), (b)[buf_len(b)] = (x), buf__hdr(b)->len++)
+#define buf_last(b) ((b)[buf_len(b)-1])
 #define buf_free(b) ((b) ? free(buf__hdr(b)) : 0)
 
 static void *buf__grow_f(const void *buf, size_t new_len, size_t elem_size)
